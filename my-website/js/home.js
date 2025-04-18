9dd7190bc96d69044000f9f682b53394a3f6e0ae
masternaip <167619502+masternaip@users.noreply.github.com>
2025-04-18 21:29:47 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'd0a9548a34ac3ef26148ec87f5f35618';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/w500';
     let currentItem;
