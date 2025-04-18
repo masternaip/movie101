7a2da3132a82179d1121eed6f4a7135fd9914d12
masternaip <167619502+masternaip@users.noreply.github.com>
2025-04-18 22:02:56 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,10 +1,10 @@
-const API_KEY = 'd0a9548a34ac3ef26148ec87f5f35618';
-    const BASE_URL = 'https://api.porndb.me/pornsites';
-    const IMG_URL = 'https://image.porndb.me/pornsites';
+const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+    const BASE_URL = 'https://api.themoviedb.org/3';
+    const IMG_URL = 'https://image.tmdb.org/t/p/w500';
     let currentItem;
 
     async function fetchTrending(type) {
-      const res = await fetch(`${BASE_URL}/pornsites/${type}/week?api_key=${API_KEY}`);
+      const res = await fetch(`${BASE_URL}/trending/${type}/week?api_key=${API_KEY}`);
       const data = await res.json();
       return data.results;
     }
